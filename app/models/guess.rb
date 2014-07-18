2949691a04b723bd12406dcbc96fa88adf406c5e
Julius Jung <juljun14@gmail.com>
2014-07-18 12:41:48 -0400

    Revert "Guess Migration and Model"

--- app/models/guess.rb
+++
@@ -1,4 +0,0 @@
-class Guess < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :card
-end
--- db/migrate/20140718121050_create_guesses.rb
+++
@@ -1,9 +0,0 @@
-class CreateGuesses < ActiveRecord::Migration
-  def change
-  	  create_table :guesses do |t|
-       t.boolean :type
-       t.references :user
-       t.references :card
-       t.timestamps
-  end
-end
