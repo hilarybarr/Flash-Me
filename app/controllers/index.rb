05a012470653f12d2ad609527980647bc921ac25
Dylan Krause <dykrause@gmail.com>
2014-07-19 15:28:23 -0400

    Move Session Controller out of Index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,37 +5,6 @@ get '/' do
   erb :index
 end
 
-#----------- SESSIONS -----------
-
-get '/sessions/new' do
-  # render sign-in page
-  @email = nil
-  erb :sign_in
-end
-
-post '/sessions' do
-  # sign-in
-  @email = params[:email]
-  user = User.where(email: @email).first
-  if params[:password] == user[:password]
-    # successfully authenticated; set up session and redirect
-    session[:user_id] = user.id
-    redirect '/decks'
-  else
-    # an error occurred, re-render the sign-in form, displaying an error
-    @error = "Invalid email or password."
-    erb :sign_in
-  end
-end
-
-delete '/sessions/:id' do
-  # sign-out -- invoked via AJAX
-  return 401 unless params[:id].to_i == session[:user_id].to_i
-  session.clear
-  200
-end
-
-
 #----------- USERS -----------
 
 get '/users/new' do
