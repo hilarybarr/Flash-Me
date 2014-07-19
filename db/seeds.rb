d95abcef2ecdd5a0e4b6c55dfcbaceeb5a9f651d
Dylan Krause <dykrause@gmail.com>
2014-07-19 16:59:04 -0400

    Setup for Begin Round Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require "csv"
 
 file = File.open("lib/world_capitals.txt")
 temp = []
-Deck.create(name: "World Capitals")
+Deck.create(name: "Capitals of The World")
 file.each do |row|
   temp << row.split("\r")
   @pairs = temp.each_slice(1).to_a.flatten.flatten.each_slice(1).to_a
@@ -14,7 +14,7 @@ end
 @pairs.each do |pair|
   for each_object in pair
     splitted_pair = each_object.split(',')
-    Card.create(hint: splitted_pair[0],answer: splitted_pair[1],deck_id: Deck.find_by_name("World Capitals").id)
+    Card.create(hint: splitted_pair[0],answer: splitted_pair[1],deck_id: Deck.find_by_name("Capitals of The World").id)
   end
 end
 
