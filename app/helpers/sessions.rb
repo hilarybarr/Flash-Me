5907fa0bacd6b2ef513d35a652d7101ceaee44be
Dylan Krause <dykrause@gmail.com>
2014-07-19 15:19:50 -0400

    Round Helper Method

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -4,4 +4,8 @@ helpers do
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_round
+  	@round ||=Round.find(session[:round_id]) if sessions[:round_id]
+  end 
+
 end
