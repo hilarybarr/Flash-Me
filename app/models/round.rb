45d21aadd099aacc310b5a901a4e96ee0fd5a1af
Dylan Krause <dykrause@gmail.com>
2014-07-19 18:39:39 -0400

    add playable_card method and assoc

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,8 @@ class Round < ActiveRecord::Base
   belongs_to :user
   belongs_to :deck
   has_many :guesses
+  has_many :guessed_cards, through: :guesses, source: :card 
+  def playable_cards 
+  	deck.cards.select{|card| card unless guessed_cards.include? card}
+  end
 end
