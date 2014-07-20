711d2691e3fddd6e0d9b1999c9d657944419bce7
Dylan Krause <dykrause@gmail.com>
2014-07-20 15:34:45 -0400

    Align Card Controller Code with Solution Controller Code

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -9,20 +9,21 @@ end
 
 post "/card/solution/:id" do
 	puts "Kello"
-	card = Card.find(params[:id])
+	@card = Card.find(params[:id])
 	guess = params[:guess] 
-	if guess == card.answer
+	if guess == @card.answer
 		correct = true 
 		current_round.increment!(:correct_guess_count)
 	else
 		correct = false
+		#current_round.guess_count -= 1
 	end
-	Guess.create(correct: correct, 
+	@guess = Guess.create(correct: correct, 
 				 user_id: current_user.id, 
 				 card_id: params[:id], 
-				 round_id: session[:round])
-
-	redirect "/card_solution/#{card.id}"	
+				 round_id: session[:round_id])
+	puts @guess
+	redirect "/card_solution/#{@card.id}"	
 end 
 
 
