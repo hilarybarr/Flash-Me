48f5449549e0670c63d5d8b797b4c5ad1df9d86b
Dylan Krause <dykrause@gmail.com>
2014-07-20 16:52:25 -0400

    Pre Merge Commit

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -8,7 +8,6 @@ erb :card_guess
 end 
 
 post "/card/solution/:id" do
-	puts "Kello"
 	@card = Card.find(params[:id])
 	guess = params[:guess] 
 	if guess == @card.answer
@@ -16,13 +15,12 @@ post "/card/solution/:id" do
 		current_round.increment!(:correct_guess_count)
 	else
 		correct = false
-		#current_round.guess_count -= 1
 	end
 	@guess = Guess.create(correct: correct, 
 				 user_id: current_user.id, 
 				 card_id: params[:id], 
 				 round_id: session[:round_id])
-	puts @guess
+	
 	redirect "/card_solution/#{@card.id}"	
 end 
 
