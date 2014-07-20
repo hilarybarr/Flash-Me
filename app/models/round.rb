b015cad58fcaa0d917fbff4caf4ff79f29c3112d
Dylan Krause <dykrause@gmail.com>
2014-07-20 13:57:51 -0400

    Slight modificaiton to Round Model

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,9 +2,6 @@ class Round < ActiveRecord::Base
   # Remember to create a migration!
   attr_accessor :guess_count
 
-  def initialize
-  	@guess_count=3
-  end
 
   belongs_to :user
   belongs_to :deck
