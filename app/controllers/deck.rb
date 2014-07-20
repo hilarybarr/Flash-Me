8ebc8dbec89a21f1f9895007235d9de9e51fb7d2
hilarybarr <hilaryrosebarr@gmail.com>
2014-07-20 11:25:14 -0400

    Added correct_guess_count incrementor and removed random card generation

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -6,27 +6,20 @@ get "/decks" do
   erb :decks
 end
 
+
 get "/game_deck/:id" do 
 	@deck = Deck.find(params[:id])
 	erb :round_intro
 end
 
+
 post "/deck/setup" do 
+	@deck = Deck.find(params[:deck].to_i)
+	@round = Round.create(
+				 correct_guess_count: 0, 
+				 deck_id: @deck.id,
+				 user_id: current_user)
 
-	puts params.inspect
-
-@deck = Deck.find(params[:deck].to_i)
-
-@round = Round.create(
-			 correct_guess_count: 0, 
-			 deck_id: @deck.id,
-			 user_id: current_user)
-
-session[:round_id] = @round.id
-
-
-
-redirect "/card/guess"
-
-
+	session[:round_id] = @round.id
+	redirect "/card/guess"
 end
