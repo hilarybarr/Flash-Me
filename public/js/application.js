65c3ad81f03f02cca7a300c2d4cad8e083774e50
Joel Yawili <elzyawili@gmail.com>
2014-07-20 20:33:16 -0400

    modified intro page/added js effects on intro page and other stuff.

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
 
   // send an HTTP DELETE request for the sign-out link
+ $("nav").fadeIn(3000);
   $('a#sign-out').on("click", function (e) {
     e.preventDefault();
     var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
     request.done(function () { window.location = "/"; });
   });
+
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1500)
+    .next()
+    .fadeIn(1500)
+    .end()
+    .appendTo('#slideshow')
+    ('slideshow').end() ;
+},  3000);
+   
+ 
 
 });
